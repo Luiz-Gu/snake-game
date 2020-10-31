3e11ac4c5108ce44769b7819bdcbf73d246d2866
Luiz Gustavo <luizrsilv26@gmail.com>
2020-10-31 17:55:26 -0300

    definir movimento da cobra

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let snake = [];
      x: 8 * box,
      y: 8 * box
  }
+ let direction = "right";
+
     function criarBG() {
         context.fillStyle = "lightgreen" //define o estilo do background
         context.fillRect(0, 0, 16 * box, 16 * box)
@@ -19,8 +21,29 @@ function criarCobrinha(){
     }
 }
 
+function iniciarJogo(){
+    criarBG()
+    criarCobrinha()
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box; //caso o quadrado va pra direita adiciona um a mais
+    if(direction == "left") snakeX -= box;  // caso ela va pra esquerda diminiu um bloco
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        X: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);//passa o intervelalo pra 100ms para tudo ser resetado e iniciar o jogo
 
 
 
-criarBG()
-criarCobrinha()
+
